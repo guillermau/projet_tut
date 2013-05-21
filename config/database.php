fbb4dd2508f141edaefaed897efa01dc93099562
thomasNicolazzi <thomas.nicolazzi@gmail.com>
2013-05-21 11:41:51 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'bddeco';
-$db['default']['password'] = 'beraledavgeo';
+$db['default']['password'] = '';
 $db['default']['database'] = 'entrepot';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
